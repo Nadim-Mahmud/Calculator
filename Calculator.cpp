73285b61fcfaf315c1ed276e5673abb47e13c330
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-05-10 10:58:50 +0600

    new line bug fixed

--- Calculator.cpp
+++ Calculator.cpp
@@ -18,7 +18,7 @@ void print(int n){
 }
 int main(){
     int i,j,k,n,m,a,b;
-    std::cout<<"Enter:\n- 1 for sum\n- 2 for subtraction\n- 3 for multiplication \- 4 for division\n";
+    std::cout<<"Enter:\n- 1 for sum\n- 2 for subtraction\n- 3 for multiplication \n- 4 for division\n";
     std::cin>>n;
     std::cin>>a>>b;
     if(n==1){
