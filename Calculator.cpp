3dd737d1a21d2f6c5fb0dd36303877c3d50a370e
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-05-10 09:53:50 +0600

    some line added to to main function

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,9 +2,17 @@
 
 
 int main(){
-    int i,j,k,n,m;
-    std::cout<<"Enter:\n-1 for sum\n-2 for subtracttion\n";
+    int i,j,k,n,m,a,b;
+    std::cout<<"Enter:\n- 1 for sum\n- 2 for subtraction\n- 3 for multiplication \- 4 for division\n";
     std::cin>>n;
-
+    std::cin>>a>>b;
+    if(n==1){
+    }
+    else if(n==2){
+    }
+    else if(n==3){
+    }
+    else{
+    }
     return 0;
 }
